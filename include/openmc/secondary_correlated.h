e372134473e0677ee3f41789b932cf2f26b496e1
Paul Romano <paul.k.romano@gmail.com>
2021-01-05 07:55:03 -0600

    Separate serialize method on angle-energy dists into serialize/flatten

--- include/openmc/secondary_correlated.h
+++ include/openmc/secondary_correlated.h
@@ -43,7 +43,9 @@ public:
   void sample(double E_in, double& E_out, double& mu,
     uint64_t* seed) const override;
 
-  UnifiedAngleEnergy serialize() const;
+  void serialize(DataBuffer& buffer) const;
+
+  UnifiedAngleEnergy flatten() const;
 
   // energy property
   std::vector<double>& energy() { return energy_; }
