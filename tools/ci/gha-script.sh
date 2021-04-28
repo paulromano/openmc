1863a69992964bc386fed3de0690e4adb0560c94
Paul Romano <paul.k.romano@gmail.com>
2021-04-28 16:30:42 -0500

    Show timings on tests

--- tools/ci/gha-script.sh
+++ tools/ci/gha-script.sh
@@ -8,11 +8,11 @@ args=" "
 if [[ $MPI == 'y' ]]; then
   args="${args} --mpi "
 fi
-  
+
 # Check for event-based
 if [[ $EVENT == 'y' ]]; then
   args="${args} --event "
 fi
 
 # Run regression and unit tests
-pytest --cov=openmc -v $args tests
+pytest --durations=0 --cov=openmc -v $args tests
