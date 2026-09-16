470cbab50248c84bc113afb7502ce5b57f178411
Paul Romano <paul.k.romano@gmail.com>
2026-09-16 11:55:38 -0500

    Simplify material library whitespace test

--- tests/unit_tests/test_material_library.py
+++ tests/unit_tests/test_material_library.py
@@ -134,42 +134,10 @@ def test_library_material_names():
     assert openmc.Material.next_id == next_id
 
 
-@pytest.mark.parametrize('stored_name, query_name', [
-    (
-        'Glass Scintillator, Li Doped (GS1, GS2, GS3)',
-        'Glass Scintillator, Li Doped  (GS1, GS2, GS3)',
-    ),
-    (
-        'Glass Scintillator, Li Doped (GS10, GS20, GS30)',
-        'Glass Scintillator, Li Doped  (GS10, GS20, GS30)',
-    ),
-    (
-        'Glass Scintillator, Li Doped (GSF1, GSF2, and GSF3)',
-        'Glass Scintillator, Li Doped  (GSF1, GSF2, and GSF3)',
-    ),
-    (
-        'Glass Scintillator, Li Doped (KG1, KG2, KG3)',
-        'Glass Scintillator, Li Doped  (KG1, KG2, KG3)',
-    ),
-    (
-        'Lutetium Yttrium OxyorthoSilicate: 0.5 atom% Cerium (LYSO)',
-        'Lutetium Yttrium OxyorthoSilicate:  0.5 atom% Cerium (LYSO)',
-    ),
-    (
-        'Radiochromic Dye Film, Nylon Base (RDF: NB)',
-        'Radiochromic Dye Film, Nylon Base  (RDF: NB)',
-    ),
-    (
-        'Tissue Equivalent-Gas, methane based (TEG: MB)',
-        'Tissue Equivalent-Gas, methane based  (TEG: MB)',
-    ),
-    (
-        'Tissue Equivalent-Gas, propane based (TEG: PB)',
-        'Tissue Equivalent-Gas, propane based  (TEG: PB)',
-    ),
-])
-def test_library_name_whitespace(stored_name, query_name):
+def test_library_name_whitespace():
     """Whitespace differences do not prevent material lookup."""
+    stored_name = 'Glass Scintillator, Li Doped (GS1, GS2, GS3)'
+    query_name = 'Glass Scintillator, Li Doped  (GS1, GS2, GS3)'
     stored = openmc.Material.from_library(stored_name)
     queried = openmc.Material.from_library(query_name)
 
